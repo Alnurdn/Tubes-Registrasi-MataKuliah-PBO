e24e3c52f3d3412643cf3bf5848035439ef78b65
ANZYM <ANZYM@AKUINI-PC>
2016-03-18 21:47:07 +0700

    MataKuliahhh

--- tubesss/src/tubesss/MataKuliah.java
+++ tubesss/src/tubesss/MataKuliah.java
@@ -7,10 +7,30 @@ package tubesss;
 
 /**
  *
- * @author ANZYM
+ * @author Rizaldi
  */
 public class MataKuliah {
-    Public MataKuliah () {
-        
+    private String namaMK;
+    private String jadwal;
+    
+    public MataKuliah (String namaMK, String jadwal) {
+        this.namaMK = namaMK;
+        this.jadwal = jadwal;
+    }
+    
+    public void setNamaMK (String namaMK) {
+        this.namaMK = namaMK;
+    }
+    
+    public void setJadwal (String jadwal) {
+        this.jadwal = jadwal;
+    }
+    
+    public String getJadwal() {
+        return jadwal;
+    }
+    
+    public String getNamaMK() {
+        return namaMK;
     }
 }
