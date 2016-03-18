53fcd8a406282961e8145f3167e71d29d8d495a5
ANZYM <ANZYM@AKUINI-PC>
2016-03-18 21:54:04 +0700

    coba kelas

---
+++ tubesss/src/tubesss/Kelas.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tubesss;
+
+/**
+ *
+ * @author ANZYM
+ */
+public class Kelas {
+    private MataKuliah matakuliah;
+    private Dosen dosen;
+    private String namaKelas;
+    private int maxMahasiswa;
+    
+    public Kelas (String namaMK,String jadwal) {
+        
+    }
+    
+    public void setDosen(Dosen D) {
+        this.dosen = dosen;
+    }
+}
