5c6740b85aa20f21879edf4de28e6bb0f3924c55
ANZYM <ANZYM@AKUINI-PC>
2016-04-01 21:10:02 +0700

    mahasiswa

--- tubesss/src/tubesss/Mahasiswa.java
+++ tubesss/src/tubesss/Mahasiswa.java
@@ -9,12 +9,16 @@ package tubesss;
  *
  * @author ANZYM
  */
+
+import java.util.*;
+
 public class Mahasiswa extends Orang {
+    private ArrayList<Kelas> listKelas = new ArrayList<>();
     private final Kelas[] pilihan;
     private int nKelas;
     private final int maxKelas;
     
-    public Mahasiswa (String nama, Long id) {
+    public Mahasiswa (String nama, int id) {
         super(nama,id);
         this.maxKelas = 10;
         this.nKelas = 0;
@@ -22,24 +26,25 @@ public class Mahasiswa extends Orang {
     }
     
     public void tambahKelas (Kelas k) {
-       for(int i=0;i<10;i++) {
-        if(pilihan[i]!=null) {
-            i++;
-        } else {
-            this.pilihan[nKelas] = k;
-            nKelas++;
-           }
-       }
+//       for(int i=0;i<10;i++) {
+//        if(pilihan[i]!=null) {
+//            i++;
+//        } else {
+//            this.pilihan[nKelas] = k;
+//            nKelas++;
+//           }
+//       }
+        listKelas.add(k);
     }
     
     public void kurangKelas(Kelas id) {
-       for (int i=0;i<10;i++) {
-        if (pilihan[i]!=null) {
-           this.pilihan[nKelas] = id;
-        nKelas--;
-        i--;
-        }
-        }
+//       for (int i=0;i<10;i++) {
+//        if (pilihan[i]!=null) {
+//           this.pilihan[nKelas] = id;
+//        nKelas--;
+//        i--;
+//        }
+//        }
+        listKelas.remove(id);
     }
-    
 }
