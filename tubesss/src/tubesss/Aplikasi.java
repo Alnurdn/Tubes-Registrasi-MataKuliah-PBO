0daee37417bfe7c55177e98cb64534669c57335e
ANZYM <ANZYM@AKUINI-PC>
2016-04-02 12:23:38 +0700

    Kelas Aplikasi

---
+++ tubesss/src/tubesss/Aplikasi.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tubesss;
+
+/**
+ *
+ * @author ANZYM
+ */
+public class Aplikasi {
+    private Dosen[] daftarDosen;
+    private Mahasiswa[] daftarMahasiswa;
+    private kelas daftarKelas;
+    
+    public addDosen (Dosen Dsn) {
+        
+    }
+    
+    public addMahasiswa (Mahasiswa Mhs) {
+        
+    }
+    
+    public Mahasiswa getMahasiswa (int id) {
+        
+    }
+    
+    public void deleteMahasiswa (int id) {
+        
+    }    
+}
